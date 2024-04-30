8792513744545b7a5561be4c6279793b23491007
TommasoGalistu <tommasogalistu92@gmail.com>
2024-04-30 15:34:40 +0200

    creazione funzione per incremento

--- javascript/java.js
+++ javascript/java.js
@@ -1,9 +1,14 @@
-// let incremento= document.querySelector('#incr');
-// let decremento= document.querySelector('#decr')
-// let numeroDaIncrementare= document.querySelector('#numero')
+let incremento= document.querySelector('#incr');
+let decremento= document.querySelector('#decr')
+let numeroDaIncrementare= document.querySelector('#numero')
 
 let num= 0;
-num=1
+
+incremento.addEventListener('click', () =>{
+    num++
+    numeroDaIncrementare.textContent= num
+})
+
 
 console.log(num)
 
