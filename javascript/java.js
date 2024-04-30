6cb61efdc6604dd7a4793e3e6834c3a81392e867
TommasoGalistu <tommasogalistu92@gmail.com>
2024-04-30 15:37:54 +0200

    collegamento del tasto decremento e reset

--- javascript/java.js
+++ javascript/java.js
@@ -1,6 +1,7 @@
 let incremento= document.querySelector('#incr');
-let decremento= document.querySelector('#decr')
-let numeroDaIncrementare= document.querySelector('#numero')
+let decremento= document.querySelector('#decr');
+let resetta=document.querySelector('#res')
+let numeroDaIncrementare= document.querySelector('#numero');
 
 let num= 0;
 
@@ -9,6 +10,14 @@ incremento.addEventListener('click', () =>{
     numeroDaIncrementare.textContent= num
 })
 
+decremento.addEventListener('click', () =>{
+    num--
+    numeroDaIncrementare.textContent= num
+})
 
-console.log(num)
+resetta.addEventListener('click', () =>{
+    num=0
+    numeroDaIncrementare.textContent= num
+})
+
 
