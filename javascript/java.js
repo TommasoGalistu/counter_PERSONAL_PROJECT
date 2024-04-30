462a04005ea36b9c0294f760a22965df1850517b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-04-30 15:52:03 +0200

    aggiunta di funzione per i colori ma poco funzionante
    approfondirò gli argomenti per cambiare la funzione

--- javascript/java.js
+++ javascript/java.js
@@ -2,6 +2,7 @@ let incremento= document.querySelector('#incr');
 let decremento= document.querySelector('#decr');
 let resetta=document.querySelector('#res')
 let numeroDaIncrementare= document.querySelector('#numero');
+let bottoni=document.querySelector('button')
 
 let num= 0;
 
@@ -18,6 +19,16 @@ decremento.addEventListener('click', () =>{
 resetta.addEventListener('click', () =>{
     num=0
     numeroDaIncrementare.textContent= num
+
 })
 
+
+// bottoni.addEventListener('click', () => {
+
+//     if(numeroDaIncrementare.textContent < 0){
+//         numeroDaIncrementare.style.color= 'red'
+//     }else if(numeroDaIncrementare.textContent > 0){
+//         numeroDaIncrementare.style.color= 'green'
+//     }
+// })
 
